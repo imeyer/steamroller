aa73ac8e99bcf512feb5358569cc0abb64c5e987
hoverlover <hoverlover@gmail.com>
2010-11-06 23:29:32 -0500

    Seems to make more sense as an instance method of Hash.

--- lib/steamroller.rb
+++ lib/steamroller.rb
@@ -1,18 +1,20 @@
-class Steamroller
-  def self.flatten(hash, joiner=".", &block)
-    flat = self.combine_keys(hash, joiner, [], &block).flatten
+class Hash
+  def flatten(joiner=".", &block)
+    flat = combine_keys(self, joiner, [], &block).flatten
     Hash[*flat.map {|f| f.to_a}.flatten]
   end
   
-  private
-    def self.combine_keys(data, joiner=".", prefix=[], &block)
-      if data.respond_to?(:keys)
-        data.keys.collect do |key|
-          combine_keys(data[key], joiner, [prefix, key], &block)
-        end.flatten
-      else
-        return nil if block_given? && !yield(data)
-        [prefix.flatten.join(joiner) => data]
-      end
+private
+  def combine_keys(data, joiner=".", prefix=[], &block)
+    if data.respond_to?(:keys)
+      data.keys.collect do |key|
+        combine_keys(data[key], joiner, [prefix, key], &block)
+      end.flatten
+    else
+      return nil if block_given? && !yield(data)
+      require 'ruby-debug'
+      debugger
+      [prefix.flatten.join(joiner) => data]
     end
+  end
 end
