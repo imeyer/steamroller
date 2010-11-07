81c0bbed9cc364d4d5aed4524c0e3b4cf2d96450
Ian Meyer <ianmmeyer@gmail.com>
2010-11-06 22:42:29 -0400

    Update testing for specifying the delimiter

--- steamroller.gemspec
+++ steamroller.gemspec
@@ -14,16 +14,17 @@ Gem::Specification.new do |s|
   s.email = %q{ianmmeyer@gmail.com}
   s.extra_rdoc_files = [
     "LICENSE",
-     "README.rdoc"
+     "README.markdown"
   ]
   s.files = [
     ".document",
      ".gitignore",
      "LICENSE",
-     "README.rdoc",
+     "README.markdown",
      "Rakefile",
      "VERSION",
      "lib/steamroller.rb",
+     "steamroller.gemspec",
      "test/helper.rb",
      "test/test_steamroller.rb"
   ]
--- test/test_steamroller.rb
+++ test/test_steamroller.rb
@@ -4,12 +4,16 @@ class TestSteamroller < Test::Unit::TestCase
   context "A steamrolled hash" do
     setup do
       @data = {"a" => {"b" => 3}, "c" => {"d" => 14}}
-
-      @flattened = Steamroller.flatten(@data)
     end
-  
+
     should "flatten out nicely" do
+      @flattened = Steamroller.flatten(@data)
       assert_equal Hash["c.d"=>14, "a.b"=>3], @flattened
+    end
+
+    should "flatten out nicely with a | as the delimiter" do
+      @flattened = Steamroller.flatten(@data, '|')
+      assert_equal Hash["c|d"=>14, "a|b"=>3], @flattened
     end
   end
 end
