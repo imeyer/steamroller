19888bab78b198f1402d56a0ba4657395c3f4961
Ian Meyer <ianmmeyer@gmail.com>
2010-11-07 19:55:58 -0500

    Remove unnecessary debugging code

--- lib/steamroller.rb
+++ lib/steamroller.rb
@@ -12,16 +12,6 @@ private
       end.flatten
     else
       return nil if block_given? && !yield(data)
-      begin
-        require 'ruby-debug'
-      rescue LoadError
-        if RUBY_VERSION.to_f == 1.9
-          rdebug_version = "ruby-debug19"
-        else
-          rdebug_version = "ruby-debug"
-        end
-        puts "Missing ruby-debug. Install with: gem install #{rdebug_version}"
-      end
       [prefix.flatten.join(joiner) => data]
     end
   end
